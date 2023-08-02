65b5e9fbc33d84391ac21a89737ba3a21eef4334
UrosZikic <uroszikic1512988@gmail.com>
2023-08-02 23:45:53 +0200

    site optimization

--- tools/scripts/footer.js
+++ tools/scripts/footer.js
@@ -7,9 +7,11 @@ let footerHtmlContentCenter = ` <div class="newsletter"> <label for="email">Subs
 let footerHtmlContentRight = `<div>
 <a href="https://www.linkedin.com/in/uro%C5%A1-%C5%BEiki%C4%87-78010819a/" target="_blank"> 
 <ion-icon name="logo-linkedin"></ion-icon>
+linkedin
 </a>
 <a href="https://github.com/UrosZikic" target="_blank"> 
 <ion-icon name="logo-github"></ion-icon>
+github
 </a>
 </div>`;
 let footerContainerLeft = document.createElement("div");
